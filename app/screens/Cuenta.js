cab68fad69cd0200aae6aefeb0926146233e4255
Ova.R <69318757+OvaR82@users.noreply.github.com>
2021-10-15 16:15:49 -0300

    Primera Aprox

---
+++ app/screens/Cuenta.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {View, Text} from "react-native";
+
+export default function Cuenta() {
+    return (
+        <View>
+            <Text>Cuenta...</Text>
+        </View>
+    );
+}
+
